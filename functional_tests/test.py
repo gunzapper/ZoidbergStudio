74e72a23f1c9a0b67ab4742b9846c53bc9e6d1fd
Pietro Brunetti <gunzapper@gmail.com>
2015-07-02 15:53:03 +0200

    First FT specced out in comments, and now uses unittest

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,44 +1,58 @@
 # Good news everyone
 
-# Prof Fansworth wants to access the Dr Zoidberg
-# sites, to see his medical data.
-# He goes to the site.
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-# He notice that it is impossible to access
-# without credetial. It's his first time
-# (but Fansworth not ever has a good memories)
-# He looks that, after the title box, there are two links
-# "Sing Up" and "Log In".
-# He chooses to push "Sign up"
+    def tearDown(self):
+        self.browser.quit()
 
-# He inserts his data on form.
-# Zoidberg is very unprepared
-# and the site's application form
-# require only a Nickname and
-# a password.
+    def test_first_access_to_site(self):
+        # Prof Fansworth wants to access the Dr Zoidberg
+        # sites, to see his medical data.
+        # He goes to the site.
+        self.browser.get('http://localhost:8000')
+        # Fansworth notices that the title is
+        # right
+        self.assertIn("Zoidberg's Studios", self.browser.title)
 
-# After registration Fansworth goes again to the
-# main page, and insert his credetials
+        # He notice that it is impossible to access
+        # without credetial. It's his first time
+        # (but Fansworth not ever has a good memories)
+        # He looks that, after the title box, there are two links
+        # "Sing Up" and "Log In".
+        # He chooses to push "Sign up"
 
-# Fry does not has credetial
-# and does access the site
+        # He inserts his data on form.
+        # Zoidberg is very unprepared
+        # and the site's application form
+        # require only a Nickname and
+        # a password.
 
-# Fansworth looks a slide on the left
-# and the medical data on the right.
+        # After registration Fansworth goes again to the
+        # main page, and insert his credetials
 
-# Fansworth looks that Zoiberg makes an error
-# inserting data. Fansworth tryes to modifies data.
+        # Fry does not has credetial
+        # and does access the site
 
-# To be sure that the modifications had registered
-# Fansworth colose his browser, access again
-# and look the data.
+        # Fansworth looks a slide on the left
+        # and the medical data on the right.
 
-# All is fine, happy, Fansworth goes to take a nap
-# on his fling cy, Fansworth goes to take a nap
-# on his fling couch.
+        # Fansworth looks that Zoiberg makes an error
+        # inserting data. Fansworth tryes to modifies data.
+
+        # To be sure that the modifications had registered
+        # Fansworth colose his browser, access again
+        # and look the data.
+
+        # All is fine, happy, Fansworth goes to take a nap
+        # on his fling cy, Fansworth goes to take a nap
+        # on his fling couch.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
