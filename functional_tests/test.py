05f2f3fa10323687539de28aa4ce559a6dfdf002
Pietro Brunetti <gunzapper@gmail.com>
2015-07-03 16:06:43 +0200

    Functional test now checks we can input a to-do item

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,6 +1,7 @@
 # Good news everyone
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,6 +21,12 @@ class NewVisitorTest(unittest.TestCase):
         # Fansworth notices that the title is
         # right
         self.assertIn("Zoidberg's Studios", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("Zoidberg's Studios", header_text)
+
+        ##-----------------------------------------------------------------
+        ## I develop authorization after
+        ## or use the admin back-end
 
         # He notice that it is impossible to access
         # without credetial. It's his first time
@@ -39,12 +46,37 @@ class NewVisitorTest(unittest.TestCase):
 
         # Fry does not has credetial
         # and does access the site
+        ##---------------------------------------------------------
 
         # Fansworth looks a slide on the left
+
         # and the medical data on the right.
+        table = self.browser.find_element_by_id('id_metadata_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == 'default metadata here' for row in rows)
+        )
+
+        inputbox = self.browser.find_element_by_id('id_dicom_metadata')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'default metadata here'
+        )
 
         # Fansworth looks that Zoiberg makes an error
         # inserting data. Fansworth tryes to modifies data.
+        inputbox.send_keys("Good news every one")
+        inputbox.send_keys(Keys.ENTER)
+
+        ## Probably it is better a button
+        ## when pushed the sites updates the metadata
+        ## And reload the metadata to the box
+
+        # Fansworth look that the table is changed
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == 'Good news every one' for row in rows)
+        )
 
         # To be sure that the modifications had registered
         # Fansworth colose his browser, access again
