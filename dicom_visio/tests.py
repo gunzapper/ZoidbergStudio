e684038dfbeaad693929da90d5186e1d3727fad9
Pietro Brunetti <gunzapper@gmail.com>
2015-07-03 12:01:09 +0200

    Basic view now returns minimal HTML

--- dicom_visio/tests.py
+++ dicom_visio/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from dicom_visio.views import home_page
 
 class HomePageTest(TestCase):
@@ -8,3 +10,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
 
         self.assertEqual(found.func.__name__, 'home_page')
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b"<title>Zoidberg's Studios</title>", response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
